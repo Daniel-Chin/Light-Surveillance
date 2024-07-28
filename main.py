d90fb271126a3d400a01df71892e1e2fc180f443
Daniel Chin <daniel.chin@nyu.edu>
2024-07-28 12:59:12 +0800

    complet. field test passed.

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from contextlib import contextmanager
+import typing as tp
+import time
+
 import numpy as np
 import cv2
 
 from median_filter import MedianFilter
 
-FILTER_SEC = 1.0
+CHANGE_THRESHOLD = 0.5
+
+FILTER_SEC = 0.0
+FILTER_N_FRAMES = 3
 TAPE_SEC = 5.0
-STAY_AWAKE_SEC = 5.0
+STAY_AWAKE_SEC = 30.0
+STAY_AWAKE_SEC = 3.0
 
 # FOUR_CC, EXT = 'XVID', 'avi'
 # FOUR_CC, EXT = 'H264', 'mp4'
@@ -28,36 +35,28 @@ def VideoWriter(*args, **kw):
     finally:
         out.release()
 
-class CircularBuffer:
-    def __init__(self, n_frames: int, width: int, height: int):
-        self.n_frames = n_frames
-        self.buffer = np.zeros((
-            n_frames, height, width, 
-        ), dtype=np.uint8)
-        self.push_i = 0
-        self.pop_i = 0
-    
-    def push(self, frame: np.ndarray):
-        self.buffer[self.push_i] = frame
-        self.push_i = (self.push_i + 1) % self.n_frames
-        if self.push_i == self.pop_i:
-            self.pop_i = (self.pop_i + 1) % self.n_frames
-    
-    def Harvest(self):
-        while self.pop_i != self.push_i:
-            yield self.buffer[self.pop_i]
-            self.pop_i = (self.pop_i + 1) % self.n_frames
-
-def FramesInCap(cap: cv2.VideoCapture, apart: int):
-    assert isinstance(apart, int) and apart >= 1
-    buf = []
+def LastFramesInCap(
+    cap: cv2.VideoCapture, fps: float, 
+    out: tp.List[np.ndarray], 
+):
+    assert not out
+    tape_len = round(TAPE_SEC * fps)
     while True:
         try:
             feed_is_alive, frame = cap.read()
             assert feed_is_alive
-            buf.append(frame)
-            if len(buf) == apart + 1:
-                yield frame, buf.pop(0)
+            for thickness, color in ((3, 0), (2, 255)):
+                cv2.putText(
+                    frame, time.ctime(), (10, 50), 
+                    cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.0, 
+                    color=(color, color, color), 
+                    thickness=thickness, 
+                    lineType=cv2.LINE_AA, 
+                )
+            out.append(frame)
+            if len(out) > tape_len:
+                out.pop(0)
+                yield
         except KeyboardInterrupt:
             print('bye')
             break
@@ -69,16 +68,33 @@ def main():
         height = round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         fps    = round(cap.get(cv2.CAP_PROP_FPS))
         size = (width, height)
-        medFilter = MedianFilter(round(FILTER_SEC * fps))
-        contextBuf = CircularBuffer(round(CONTEXT_PAD_SEC * fps), *size)
+        medFilter = MedianFilter(FILTER_N_FRAMES + round(FILTER_SEC * fps))
         fourcc: int = cv2.VideoWriter_fourcc(*FOUR_CC)
         with VideoWriter(f'./out.{EXT}', fourcc, fps, size) as out:
             sleeping, inactivity = False, 0.0
-            last_frame = np.zeros((height, width, 3), dtype=np.uint8)
-            for frame in FramesInCap(cap):
-                change = (np.abs(frame - last_frame).astype(np.float32) / 256.0).mean()
-                print(f'{change : .2e}')
-                last_frame = frame
+            tape: tp.List[np.ndarray] = []
+            print('Starting...')
+            for _ in LastFramesInCap(cap, fps, tape):
+                change = (np.abs(tape[-1] - tape[0]).astype(np.float32) / 256.0).mean()
+                smoothed_change = medFilter.next(change)
+                if smoothed_change is None:
+                    continue
+                # print(f'\r {smoothed_change = :.5f} {inactivity = :.1f}', end='', flush=True)
+                if sleeping:
+                    if smoothed_change > CHANGE_THRESHOLD:
+                        # print('\n waking up')
+                        sleeping, inactivity = False, 0.0
+                        for frame in tape:
+                            out.write(frame)
+                else:
+                    out.write(tape[-1])
+                    if smoothed_change > CHANGE_THRESHOLD:
+                        inactivity = 0.0
+                    else:
+                        inactivity += 1 / fps
+                        if inactivity > STAY_AWAKE_SEC:
+                            # print('\n going to sleep')
+                            sleeping, inactivity = True, -1.0
 
 if __name__ == "__main__":
     main()
